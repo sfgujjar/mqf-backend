13543687fffd7a88898393c860f74f7ce1c803a6
sfgujjar <167501150+sfgujjar@users.noreply.github.com>
2025-10-05 23:25:19 +0500

    server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 app.use('/api/user', userRoutes);
 
 // ✅ MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/mqf', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
