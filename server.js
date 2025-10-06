37da593b7b4ac6166c062ff55a0434d3ea78f17d
sfgujjar <167501150+sfgujjar@users.noreply.github.com>
2025-10-07 00:06:13 +0500

    server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // ✅ Server Start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || ;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
