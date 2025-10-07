dc0825f32b11f5596a6dd5cbc564148d505b7b18
sfgujjar <167501150+sfgujjar@users.noreply.github.com>
2025-10-07 10:27:09 +0500

    server.js

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use('/api/user', userRoutes);
 
 // ✅ MongoDB Connection
 console.log("Mongo URI:", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => {
   console.log('MongoDB connected successfully');
 }).catch((err) => {
