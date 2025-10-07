16ba4ce52d37575d40d45844262a3461b3494276
sfgujjar <167501150+sfgujjar@users.noreply.github.com>
2025-10-07 09:49:48 +0500

    server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('MongoDB connected successfully');
 }).catch((err) => {
   console.error('MongoDB connection error:', err);
+     process.exit(1); // graceful shutdown
 });
 
 // ✅ Server Start
